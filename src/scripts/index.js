326ca7c07aac4c2cf1be407f14aba5371487ed2f
Stanyslas Bres <stanyslas.bres@gmail.com>
2019-05-05 01:57:02 +0200

    build(styles): import Bootstrap's styles from sass

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,8 @@
 // DEPENDENCIES IMPORTS (jquery and lodash are loaded globally with the webpack provide plugin)
 import 'jquery.scrollto';
-import 'bootstrap/dist/js/bootstrap.min';
+import 'bootstrap';
 
 // STYLES DEPENDENCIES IMPORTS
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/main.scss';
 
 console.log(`JQuery version : ${jQuery.fn.jquery}`);
